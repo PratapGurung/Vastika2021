79a69afda2c802a0e09f5c59cbcd7495714ed4b7
Pratap Gurung <pratapgurung2071@gmail.com>
2021-01-26 00:04:32 -0500

    Question 11 to 20 submitted.

--- src/JavaQuestion11To20.java
+++ src/JavaQuestion11To20.java
@@ -7,7 +7,11 @@ public class JavaQuestion11To20 {
 		// TODO Auto-generated method stub
 		//call here any method to test 
 		//displayiInfo();
-		ascii(12);
+		//ascii(12);
+		//leapYear(1992);
+		//largestNum(10,21,3);
+		//triangleInfo(2,3,4);
+		calculator();
 	}
 	
 	/*
@@ -167,11 +171,137 @@ public class JavaQuestion11To20 {
 		
 	}
 	
+	/*
+	 * question 16
+	 * find the given number is even or odd.
+	 * to call this function: parity(int)
+	 * return: void
+	 * return type: void
+	 */
 	
+	public static void parity(int num) {
+		if(num % 2 == 0 ) {
+			System.out.printf("Number %d is even.", num);
+		}
+		else {
+			System.out.printf("Number %d is odd.", num);
+		}
+	}
 	
+	/*
+	 * question 17
+	 * description: calculate leap year.
+	 * to call this function: leapYear(int)
+	 *
+	 * return type: boolean
+	 */
 	
+	public static boolean leapYear(int year) {
+		boolean value = false;
+		if(year%4 == 0) {
+			if(year%100 ==0) {
+				if(year%400 ==0) {
+					System.out.printf("Year %d is a leap year.", year);
+				}
+				else {
+					System.out.printf("Year %d is not a leap year.", year);
+				}
+			}
+			else {
+				System.out.printf("Year %d is a leap year.", year);
+			}
+		}
+		else {
+			System.out.printf("Year %d is not a leap year.", year);
+		}
+		return value;
+	}
 	
+	/*
+	 * question 18
+	 * description: display the largest number from given three values.
+	 * to call this function: largestNum(int, int , int)
+	 *
+	 * return type: int 
+	 */
+	public static int largestNum(int a, int b, int c ) {
+		int largest = 0;
+		if(a > b && a > c) {largest = a;}
+		if(b > a && b > c) {largest = b;}
+		if(c > a && c > b) {largest = c;}
+		System.out.printf("Largest num out of %d, %d, %d is %d.", a,b,c,largest);
+		return largest;
+	}
 	
+	/*
+	 * question 19
+	 * description: accept three sides of a triangle and display which kind of triangle is formed.
+	 * to call this function: triangleInfo(int, int , int)
+	 *
+	 * return type: void 
+	 */
+	public static void triangleInfo(int a, int b, int c) {
+		if(a == b && b == c ) {
+			System.out.println("Equilateral  Triangle");
+		}
+		else if(a == b || b == c || a == c) {
+			System.out.println("Isosceles  Triangle");
+		}
+		else {
+			System.out.println("Scalene Triangle");
+		}
+	}
 	
-	
+	/*
+	 * question 20
+	 * description: create the equivalent of a four-function calculator. 
+	 * to call this function: calculator()
+	 *
+	 * return type: void 
+	 */
+	public static void calculator() {
+		Scanner input = new Scanner(System.in);
+		try {
+		
+			System.out.println("==========================Calculator===========================");
+			System.out.println("|	Functions: 						 	          ");
+			System.out.println("| 	1 to add.											  ");
+			System.out.println("|	2 to subtract.										  ");
+			System.out.println("| 	3 to multiply.											  ");
+			System.out.println("|	4 to divide.										  ");
+			System.out.println("|															  ");
+			System.out.println("===============================================================");
+			
+			System.out.print("Enter first number: ");
+			double num1 = input.nextDouble();
+			System.out.print("Enter second number: ");
+			double num2 = input.nextDouble();
+			System.out.print("Enter first number: ");
+			int function = input.nextInt();
+			double result = 0;
+			switch(function){
+			case 1:
+				result = num1 + num2;
+				System.out.printf("Addition of %.0f and %.0f is %.0f", num1, num2, result);
+				break;
+			case 2:
+				result = num1 - num2;
+				System.out.printf("Subtraction of %.0f from %.0f is %.0f", num1, num2, result);
+				break;
+			case 3:
+				result = num1 * num2;
+				System.out.printf("Multiplication of %.0f and %.0f is %.0f", num1, num2, result);
+				break;
+			case 4: 
+				result = num1 / num2;
+				System.out.printf("Division of %.0f by %.0f is %.2f", num1, num2, result);
+				break;
+			}
+			input.close();
+		}
+		catch(InputMismatchException e){
+			System.out.println("Please enter number only.");
+		}
+
+	}
 }
